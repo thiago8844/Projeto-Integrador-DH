7e81b7241c90384e3e6679a51de6988e43c13484
Thiago Machado <thiagoaxe4@gmail.com>
2023-02-02 21:32:25 -0300

    Juntei a monte seu pc e a home

---
+++ home/javascript/slider.js
@@ -0,0 +1,8 @@
+$('.slider-container').slick({
+  infinite: true,
+  speed: 450,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  prevArrow: '<span class="prev_arrow"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="seta-volta"><path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" /></svg></span>',
+  nextArrow: '<span class="next_arrow"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="seta-avanca"><path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" /></svg></span>',
+});
